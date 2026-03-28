657f242fb7f86207fd411ecbd8a7b264880acdd0
Kaushik Biswas <kash2023k@gmail.com>
2026-03-28 16:25:25 +0530

    Fix Procfile for deployment

--- ai_webapp/ai_webapp/settings.py
+++ ai_webapp/ai_webapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-uj%y62*bwpdf%#2_z_r@w%sxik+7h1xb%jw+$t&dg=#d&844!("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
